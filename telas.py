d13851880379752a170a129cbf84c4136c268133
VKOF005 <vkofujihara@gmail.com>
2026-09-07 18:59:22 -0300

    Merge remote-tracking branch 'TCC/master'
    # Conflicts:
    #	kv/conecteOsConceitosTela.kv

--- telas.py
+++ telas.py
@@ -1,8 +1,19 @@
 from kivy.uix.screenmanager import Screen
+from conecteOsConceitos import ConecteOsConceitos
 
 class QuizTela(Screen):
     pass
+
 class XXX(Screen):
     pass
+
 class ConecteOsConceitosTela(Screen):
-    pass
+    game = ConecteOsConceitos()
+    
+    def decrease_counter(self):
+        self.game.root = self
+        self.game.decrease_counter()
+    
+    def increase_counter(self):
+        self.game.root = self
+        self.game.increasse_counter()
